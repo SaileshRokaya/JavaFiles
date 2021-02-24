7053116b9c485f460d6c8bebd8e1de623faf4f3e
Sailesh Rokaya <saileshrokaya123@gmail.com>
2021-02-24 08:45:02 +0545

    Second Commit

--- src/pkg1/BasicIO.java
+++ src/pkg1/BasicIO.java
@@ -1,10 +1,17 @@
 
 package pkg1;
-
+import java.util.Scanner;
 
 //Basic Input and output
 public class BasicIO {
     public void print(String msg){
         System.out.println(msg);
     }
+    
+    public String readString(){
+        return new Scanner(System.in).nextLine();
+    }
+    
+    
+    
 }
--- src/pkg2/TestIO.java
+++ src/pkg2/TestIO.java
@@ -1,10 +1,47 @@
 
 package pkg2;
 import pkg1.BasicIO;
+import pkg1.Calculator;
 
 public class TestIO {
     public static void main(String[] args) {
+        String id, name, address, num1, num2;
         BasicIO io = new BasicIO();
-        io.print("Enter your id: ");
+        
+        Calculator c2 = new Calculator();
+        int n1, n2, n3;
+        
+//        io.print("Enter your id: ");
+//        id = io.readString();
+//        io.print("Enter your Name: ");
+//        name = io.readString();
+//        io.print("Enter your Address: ");
+//        address = io.readString();
+        
+
+        //Input
+        io.print("Enter your first number: ");
+        num1 = io.readString();
+        
+        io.print("Enter your second number: ");
+        num2 = io.readString();
+        
+        
+        //Type Conversion
+        //String to Integer
+        n1= Integer.parseInt(num1);
+        n2= Integer.parseInt(num2);
+        c2.setNum1(n1);
+        c2.setNum2(n2);
+        c2.CalcSum();
+        
+        //Output
+        io.print("The sum is: "+c2.getNum3());
+        
+//        io.print("Your first number is: "+num1+ " and "+" second number is: "+num2);
+        
+        
+//        io.print("Your id is: "+id+","+"your name is: "+name+" and "+"your address is: "+address);
+        
     }
 }
